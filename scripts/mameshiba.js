b87699d846886870f860fd341872a1e834a1fe8d
yukimura1227 <takamura1227@gmail.com>
2019-05-19 11:10:24 +0900

    feat: post in probabillity function

--- scripts/mameshiba.js
+++ scripts/mameshiba.js
@@ -26,11 +26,14 @@ module.exports = function(robot) {
   // ex) mameshiba #general 50 明日は きっと 晴れ
   robot.hear(/^mameshiba\s+#(.*)\s+(\d+)\s+(.*)/i, async (res) => {
     var channel     = res.match[1];
-    var probability = res.match[2];
+    var probability = Number(res.match[2]);
     var words       = res.match[3].split(' ');
-    for(let word of words) {
-      robot.send({ room: channel }, word);
-      await sleep(3000);
+    var rand_0_99 = Math.floor( Math.random() * 100 ) ;
+    if( rand_0_99 < probability ) {
+      for(let word of words) {
+        robot.send({ room: channel }, word);
+        await sleep(3000);
+      }
     }
   });
 };
